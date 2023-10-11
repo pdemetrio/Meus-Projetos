4002147c54207a8752f9003a24627e91da4dbb84
Paulo <paulodemetrio1982@gmail.com>
2023-10-11 02:10:10 -0300

    atualizando a pasta

--- diosql.sql
+++ diosql.sql
@@ -25,8 +25,26 @@ create table favorite_food (
 );
 desc favorite_food;
 show databases;
-desc information_schema.table_constraints
+desc information_schema.table_constraints;
+select * from information_schema.table_constraints
 where CONSTRAINT_SCHEMA = "first_example";
 
 desc person;
 
+insert into person values ('5', 'Maria', 'Graças', 'F', '1959-05-02', 'rua tal', 'Santo Andre', 'SP', 'Brasil', '09172-570'),
+						  ('1', 'Paulo', 'Demetrio', 'M', '1982-11-26', 'rua Manuel', 'Santo Andre', 'SP', 'Brasil', '09172-730'),
+                          ('2', 'Meire', 'Oliveira', 'F', '1986-01-19', 'rua Manuel', 'Santo Andre', 'SP', 'Brasil', '09172-730'),
+                          ('4', 'Milena', 'Dantas', 'F', '2019-11-29', 'rua Manuel', 'Santo Andre', 'SP', 'Brasil', '09172-730');
+select * from person;
+
+delete from person where person_id = 3;
+
+desc favorite_food;
+
+insert into favorite_food values (1,'Pizza'),
+                                 (2, 'Strogonoff'),
+                                 (4, 'Salsicha'),
+                                 (5, 'Feijoada');
+								
+select * from favorite_food;
+                                   
